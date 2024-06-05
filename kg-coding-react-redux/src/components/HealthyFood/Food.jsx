e62f5d0c35e31e5f91cd9ddad1d55d71ff03f5f4
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-06-05 15:00:12 +0530

    created buy and remove feature

--- kg-coding-react-redux/src/components/HealthyFood/Food.jsx
+++ kg-coding-react-redux/src/components/HealthyFood/Food.jsx
@@ -5,6 +5,17 @@ import Error from "./Error";
 import BackButton from "../BackButton";
 const Food = () => {
   const [foodList, setFoodList] = useState(["Bread", "Milk", "Fruits"]);
+  const [showItem, setShowItem] = useState([]);
+
+  const showMethod = (item) => {
+    if (!showItem.includes(item)) {
+      setShowItem([...showItem, item]);
+    }
+    if (showItem.includes(item)) {
+      const newArr = showItem.filter((value) => value !== item);
+      setShowItem(newArr);
+    }
+  };
 
   const setList = (e) => {
     if (e.key === "Enter" && e.target.value != "") {
@@ -18,10 +29,20 @@ const Food = () => {
       <BackButton />
       <h2>Type the healthy food name and press Enter ↲</h2>
       <div className="flex flex-col items-center justify-center m-4 border p-4 rounded-lg">
-        <h2 className="">Healthy Foods</h2>
+        <h2 className="text-purple-800">Healthy Foods</h2>
         <InputFood setList={setList} />
         <hr className="h-px w-60" />
-        <FoodItem foodList={foodList} />
+        <ul className="m-0 p-0">
+          {foodList.map((item) => (
+            <FoodItem
+              item={item}
+              key={item}
+              change={showItem.includes(item)}
+              setShowItem={() => showMethod(item)}
+            />
+          ))}
+        </ul>
+
         <Error foodList={foodList} />
       </div>
     </div>
--- kg-coding-react-redux/src/components/HealthyFood/FoodItem.jsx
+++ kg-coding-react-redux/src/components/HealthyFood/FoodItem.jsx
@@ -1,14 +1,17 @@
-const FoodItem = ({ foodList }) => {
+const FoodItem = ({ item, change , setShowItem}) => {
   return (
-    <div>
-      <ul className="m-0 p-0">
-        {foodList.map((item) => (
-          <li key={item} className="flex justify-between items-center gap-40 my-2 font-bold">
-            <div>{item}</div> <button className="bg-blue-400 rounded-lg px-2 py-1 font-semibold">Buy</button>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <li
+      className={`flex justify-between items-center gap-40 my-2 font-bold p-2 rounded-lg ${
+        change ? "bg-pink-400" : "bg-white"
+      }`}
+    >
+      <div>{item}</div>{" "}
+      <button className="bg-purple-500 rounded-lg px-2 py-1 font-semibold text-white" onClick={()=>{
+        setShowItem();
+      }}>
+        {change ? "❌" : "Buy"  }
+      </button>
+    </li>
   );
 };
 
